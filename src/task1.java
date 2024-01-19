c8d55c99f2d0ed052c8773f697d02916ad0fce96
11-sg <sg4242674@gmail.com>
2024-01-19 11:54:12 +0530

    Initial commit

---
+++ src/task1.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class task1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("WELCOME TO GUESSING GAME");
+        int round=0,i,f;
+        while(true)
+        {   
+            System.out.println("How many chances you want to guess");
+            int chance=sc.nextInt();
+            System.out.println("Enter the range");
+            int min=sc.nextInt();
+            int max=sc.nextInt();
+            int rand=min+(int)(Math.random()*((max-min)+1));
+            f=0;
+            for(i=1;i<=chance;i++)
+            {
+            System.out.println("Enter the guess "+(i));
+            int guess=sc.nextInt();
+            if (guess==rand)
+            {
+                System.out.println("Congratulation you are correct!");
+                f=1;
+                break;
+            }
+            else if (guess>rand)
+            {
+                System.out.println("Too High");
+            }
+            else
+            System.out.println("Too Low");
+            }
+
+            if(f==0)
+            System.out.println("ALL CHANCES ARE OVER SORRY\nANSWER IS " +rand);
+            else{
+            System.out.println("ROUND "+(++round)+ " SCORE");
+            System.out.println("OUT OF "+chance+ " CHANCES, YOU GUSSED IN "+ i + " CHANCES");
+            }
+            System.out.println("Do you want to play again(y/n)");
+            String anser=sc.next().toLowerCase();
+            if(anser.equals("n")){
+                sc.close();
+            System.exit(0);}
+            else
+            continue;
+        }
+    }
+}
---
+++ src/task2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class task2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total number of subjects");
+        int subjects=sc.nextInt();
+        System.out.println("Enter your marks in all "+subjects+" subjects out of 100");
+        double[] mark=new double[subjects];
+        double sum=0.0;
+        for(int i=0;i<mark.length;i++)
+        {
+            mark[i]=sc.nextDouble();
+            sum+=mark[i];
+        }
+        double avg=(sum/subjects);
+        System.out.println("Sum = "+ sum+"\nAverage percentage = "+avg+ " %");
+        if (avg>=90.0)
+        System.out.println("Grade A");
+        else if(avg>=85.0)
+        System.out.println("Grade B");
+        else if(avg>=75.0)
+        System.out.println("Grade C");
+        else
+        System.out.println("Grade F");
+        sc.close();
+    }
+}
